b419cee0baabf095345575de6e99924e27cd74cd
Sma-source <rabhi.smain@gmail.com>
2021-12-08 19:38:35 +0100

    add toogle mobile menu to hide and display on click

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
 function navToggle() {
   btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
 }
 btn.addEventListener("click", navToggle);
